188055cc32173c060a373ea0f236d773bc8fe113
Cassandra Cruz <celkamada@gmail.com>
2015-08-06 16:14:25 -0700

    Made fix to require statement in index.js; Added ability to run callback to testModel

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var testProp = require('./lib/testProp.js');
-var testModel = require('./lib.testModel.js');
+var testModel = require('./lib/testModel.js');
 
 module.exports = {
   testProp: testProp,
